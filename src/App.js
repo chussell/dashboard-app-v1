08644bb827e37e00caea4fa4918255388a66be5b
Callum Hussell <chussell@thegreenbuddha.co.uk>
2018-12-26 16:37:50 +0000

    added material ui dashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Components/Dashboard';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Dashboard />
       </div>
     );
   }
